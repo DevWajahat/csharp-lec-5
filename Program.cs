903df75682bc6fa08fd2147d673070c8a6cf119b
Wajahat Abbas <155806602+DevWajahat@users.noreply.github.com>
2024-10-14 20:49:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Math obj = new Math();
+        obj.add(4,8);
+    }
+}
+
+internal class Math
+{
+    //public int add()
+    //{
+    //    int a = 5;
+    //    int b = 6;
+    //    int c = a + b;
+    //    return c;
+    //}
+
+    //public void add()
+    //{
+    //    int a = 5;
+    //    int b = 6;
+    //    int c = a + b;
+
+    //    Console.WriteLine(c);
+    //}
+
+    public void add (int a , int b)
+    {
+        Console.WriteLine(a + b);
+    }
+
+
+}
